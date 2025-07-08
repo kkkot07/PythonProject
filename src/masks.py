c67e94a8cffc041d0bffa1d1e43a0e47c0dbbb49
Walker Word <genstore@mail.ru>
2025-07-08 11:33:06 +0300

    Шаг 1: фиксация состояния проекта после предыдущей работы

---
+++ src/masks.py
@@ -0,0 +1,32 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Маскирует номер банковской карты по формату: XXXX XX** **** XXXX.
+
+    :param card_number: Номер карты (16 цифр)
+    :return: Замаскированный номер карты
+    """
+
+    if len(card_str) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+
+    part1 = card_str[:4]
+    part2 = card_str[4:6]
+    part3 = "**"
+    part4 = "****"
+    part5 = card_str[-4:]
+
+    return f"{part1} {part2}{part3} {part4} {part5}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    Маскирует номер банковского счёта по формату: **XXXX.
+
+    :param account_number: Номер счёта
+    :return: Замаскированный номер счёта
+    """
+
+    if len(acc_str) < 4:
+        raise ValueError("Номер счёта должен содержать минимум 4 цифры.")
+
+    return f"**{acc_str[-4:]}"
