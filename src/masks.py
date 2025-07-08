f45db073f922297e705f870e2326fede5b94192b
Walker Word <genstore@mail.ru>
2025-07-08 11:36:42 +0300

    Шаг 2: добавлены функции mask_account_card и get_date

--- src/masks.py
+++ src/masks.py
@@ -30,3 +30,10 @@ def get_mask_account(account_number: str) -> str:
         raise ValueError("Номер счёта должен содержать минимум 4 цифры.")
 
     return f"**{acc_str[-4:]}"
+
+
+from src.widget import mask_account_card, get_date
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+
+def mask_account_card(info: str) -> str:
+    """
+    Маскирует номер карты или счета, включая его тип (например, Visa, Maestro или 'Счет').
+
+    :param info: строка вида 'Visa Classic 7000792289606361' или 'Счет 73654108430135874305'
+    :return: строка с замаскированным номером
+    """
+    parts = info.strip().split()
+
+    if parts[0] == "Счет":
+        number = parts[1]
+        masked = get_mask_account(number)
+        return f"Счет {masked}"
+    else:
+        number = parts[-1]
+        name = " ".join(parts[:-1])
+        masked = get_mask_card_number(number)
+        return f"{name} {masked}"
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует ISO-дату (например, '2024-03-11T02:26:18.671407') в формат ДД.ММ.ГГГГ.
+
+    :param date_str: строка в ISO-формате
+    :return: строка в формате 'дд.мм.гггг'
+    """
+    dt = datetime.fromisoformat(date_str)
+    return dt.strftime("%d.%m.%Y")
